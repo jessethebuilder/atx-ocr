8979568511b0dd35bb062685b3fe8012d4317fb2
= <jessethebuilder@yahoo.com>
2019-12-05 20:13:43 -0800

    puma

--- app/models/client_keyword_match.rb
+++ app/models/client_keyword_match.rb
@@ -8,7 +8,7 @@ class ClientKeywordMatch
   field :keyword, type: String
   validates :keyword, presence: true
 
-  # field :email_sent, type: Boolean, default: false
+  field :email_sent, type: Boolean, default: false
 
   before_create :delete_conflicting_matches
 
@@ -17,7 +17,6 @@ class ClientKeywordMatch
   def delete_conflicting_matches
     # Only 1 ClientKeywordMatch can exist per MailImage. This deletes any, in the
     # event that User matches a MailImage that already matches to a new Client
-
     ClientKeywordMatch.where(mail_image_id: self.mail_image.id).destroy_all
   end
 end
